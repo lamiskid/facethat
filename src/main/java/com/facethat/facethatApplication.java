5f5f9acc5340ca5967ac19ea40aec4cfa9804828
lamiskid <aboladelawal2019@gmail.com>
2023-05-30 02:16:32 +0100

    Face that bootstrap

---
+++ src/main/java/com/facethat/facethatApplication.java
@@ -0,0 +1,29 @@
+package com.facethat;
+
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+
+public class facethatApplication extends Application<facethatConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new facethatApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "facethat";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<facethatConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final facethatConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/facethat/facethatConfiguration.java
@@ -0,0 +1,10 @@
+package com.facethat;
+
+import io.dropwizard.core.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import jakarta.validation.constraints.*;
+
+public class facethatConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
